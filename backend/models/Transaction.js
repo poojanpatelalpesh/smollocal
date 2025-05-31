eceb2dc0d6dcb8b58402db4829dd23697f0d1987
Raman1182 <159936121+Raman1182@users.noreply.github.com>
2025-05-31 19:34:52 +0530

    Update Transaction.js

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -4,7 +4,7 @@ const transactionSchema = new mongoose.Schema({
   name: String,
   phone: String,
   transactionId: String,
-  vendor: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor' },
+  vendor: { type: mongoose.Schema.Types.ObjectId, ref: 'Seller' },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Transaction', transactionSchema);
