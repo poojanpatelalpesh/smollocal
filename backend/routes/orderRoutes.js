674653ad6af50eec88be02a17c5bca2adca97bf0
Raman <stopitre36@gmail.com>
2025-06-01 14:08:50 +0530

    Update orderRoutes to include order status update endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,3 +1,4 @@
+const { placeOrder } = require('../controllers/orderController');
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
@@ -5,5 +6,6 @@ const { getOrdersForSeller, updateOrderStatus } = require('../controllers/orderC
 
 router.get('/', protect, getOrdersForSeller);
 router.put('/:orderId', protect, updateOrderStatus);
+router.post('/place', placeOrder);
 
 module.exports = router;
